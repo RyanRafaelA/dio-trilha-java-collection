ee8474023e5c2fbfa7f0a3fa28f595f4e053bd33
RyanRafaelA <ryrafael461@gmail.com>
2025-02-14 21:54:20 -0300

    Classe CarrinhoDeCompra construida

---
+++ exercicioCarrinhoCompras/src/service/CarrinhoDeCompra.java
@@ -0,0 +1,44 @@
+package service;
+
+import model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompra {
+    private List<Item> listaItem;
+
+    public CarrinhoDeCompra(){
+        listaItem = new ArrayList<>();
+    }
+
+    public void adicionar(String nome, double preco, int quantidade){
+        listaItem.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome){
+        listaItem.remove(nome);
+    }
+
+    public double calcularValorTotal(){
+        double valorCarrinho = 0;
+
+        for(Item items : listaItem){
+            double preco;
+            int quantidade;
+
+            preco  = items.getPreco();
+            quantidade = items.getQuantidade();
+
+            valorCarrinho += preco * quantidade;
+        }
+
+        return valorCarrinho;
+    }
+
+    public void exibirItems(){
+        for(Item items: listaItem){
+            System.out.println(items);
+        }
+    }
+}
